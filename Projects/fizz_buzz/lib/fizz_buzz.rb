4a7eec3d386540dfffcae8d7c618b21c213fe768
jocontreras <jen.almarasy@gmail.com>
2017-01-25 19:09:42 +0100

    fizz_buzz game initial commit

---
+++ Projects/fizz_buzz/lib/fizz_buzz.rb
@@ -0,0 +1,13 @@
+# lib/fizz_buzz.rb
+# fizz_buzz.rb = program that simulates fizz buzz game
+def fizz_buzz(number)
+  if number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  elsif number % 15 == 0
+    'fizzbuzz'
+  else
+    number
+  end
+end
