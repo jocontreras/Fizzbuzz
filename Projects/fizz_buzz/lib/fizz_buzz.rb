1abdf6c2f366def1bec2ea94ab6b62bbd094674c
jocontreras <jen.almarasy@gmail.com>
2017-01-27 08:14:34 +0100

    fizz_buzz game using two algorithms

--- Projects/fizz_buzz/lib/fizz_buzz.rb
+++ Projects/fizz_buzz/lib/fizz_buzz.rb
@@ -1,13 +1,27 @@
 # lib/fizz_buzz.rb
 # fizz_buzz.rb = program that simulates fizz buzz game
+
+##### Implementation 1 #######
+=begin
 def fizz_buzz(number)
-  if number % 3 == 0
-    'fizz'
+if number % 5 == 0 and number % 3 == 0
+    puts 'fizzbuzz'
+  elsif number % 3 == 0
+    puts'fizz'
   elsif number % 5 == 0
-    'buzz'
-  elsif number % 15 == 0
-    'fizzbuzz'
+    puts 'buzz'
   else
-    number
+    puts number
   end
+end
+=end
+
+
+#### Implementation #2 #
+
+def fizz_buzz number
+  result = ''
+  result += 'Fizz' if (number % 3).zero? #.zero? return true if the number if the result is 0
+  result += 'Buzz' if (number % 5).zero? #.zero? return true if the number if the result is 0
+  result.empty? ? number : result # using ternary operator ?:
 end
