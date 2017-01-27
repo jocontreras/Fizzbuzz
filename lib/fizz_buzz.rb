da7fa9fcd522b4a700a3c6ae0303f1d2c11a6c63
jocontreras <jen.almarasy@gmail.com>
2017-01-27 10:47:17 +0100

    fizz_buzz exercise final

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,32 @@
+# lib/fizz_buzz.rb
+# fizz_buzz.rb = program that simulates fizz buzz game
+
+##### Implementation 1 #######
+
+def fizz_buzz(number)
+if number % 5 == 0 and number % 3 == 0
+    puts 'fizzbuzz'
+  elsif number % 3 == 0
+    puts'fizz'
+  elsif number % 5 == 0
+    puts 'buzz'
+  else
+    puts number
+  end
+end
+
+
+
+#### Implementation #2 #
+=begin
+def fizz_buzz number
+  result = ''
+  result += 'Fizz' if (number % 3).zero? #.zero? return true if the number if the result is 0
+  result += 'Buzz' if (number % 5).zero? #.zero? return true if the number if the result is 0
+  result.empty? ? number : result # using ternary operator ?:
+end
+=end
+
+def has_zero_remainder?(number, divider)
+  number % divider == 0
+end
---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,63 @@
+require './lib/fizz_buzz'
+
+
+describe 'fizz_buzz' do
+
+  it 'returns 1 if number is 1' do
+    expect(fizz_buzz(1)).to eq 1
+  end
+
+  it "returns 'fizz' if number is divisible by 3" do
+    expect(fizz_buzz(3)).to eq 'fizz'
+  end
+
+it "returns 'buzz' if number is divisible by 5" do
+    expect(fizz_buzz(5)).to eq 'buzz'
+end
+
+it "returns 'fizzbuzz' if number is divisible by 15" do
+    expect(fizz_buzz(15)).to eq 'fizzbuzz'
+end
+end
+
+
+
+
+#### Another test approach###
+# '#' refersto an instance method's name - #fizz_buzz
+
+=begin
+describe '#fizz_buzz' do
+  it 'returns 1 if number is 1' do
+    expect(fizz_buzz 1).to eq 1
+  end
+
+  it 'returns 2 if number is 2' do
+    expect(fizz_buzz 2).to eq 2
+  end
+
+  it 'returns "Fizz" if number is divisible by 3' do
+    expect(fizz_buzz 3).to eq 'Fizz'
+  end
+
+  it 'returns "Buzz" if number is divisible by 5' do
+    expect(fizz_buzz 5).to eq 'Buzz'
+  end
+
+  it 'returns "Fizz" if number is 6' do
+    expect(fizz_buzz 6).to eq 'Fizz'
+  end
+
+  it 'returns 8 if number is 8' do
+    expect(fizz_buzz 8).to eq 8
+  end
+
+  it 'returns "Buzz" if number is 10' do
+    expect(fizz_buzz 10).to eq 'Buzz'
+  end
+
+  it 'returns "FizzBuzz" if number is divisible by 15' do
+    expect(fizz_buzz 15).to eq 'FizzBuzz'
+  end
+end
+=end
